3dacc12106530a2121ba9f1cce3a583d14300c3a
kauacrespo <kauacarlosbr3@gmail.com>
2026-06-13 12:00:43 -0300

    retactor: Anotacoes de models subistituida @Data por @Getter e @Setter

--- src/main/java/com/OrganizaDinheiro/OrganizaDinheiro/model/Expense.java
+++ src/main/java/com/OrganizaDinheiro/OrganizaDinheiro/model/Expense.java
@@ -1,9 +1,7 @@
 package com.OrganizaDinheiro.OrganizaDinheiro.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -11,7 +9,8 @@ import java.time.LocalDate;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class Expense {
 
     @Id
--- src/main/java/com/OrganizaDinheiro/OrganizaDinheiro/model/User.java
+++ src/main/java/com/OrganizaDinheiro/OrganizaDinheiro/model/User.java
@@ -1,15 +1,14 @@
 package com.OrganizaDinheiro.OrganizaDinheiro.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
